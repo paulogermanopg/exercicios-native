d20feec03c2d8b15e9517e80d89298d84d737d22
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-02-21 10:02:15 -0300

    primeiro componente

---
+++ src/componentes/Simples.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default props => 
+    <Text>Com Arrow: {props.texto}</Text>
+        
+        
+
+
+// export default function(props) {
+// return <Text>{props.texto}</Text>
+// }
