541796f62691b516108df7e88c021487a55b3092
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-02-21 14:55:16 -0300

    componente contador com cliques

--- src/Menu.js
+++ src/Menu.js
@@ -4,8 +4,12 @@ import { createDrawerNavigator } from 'react-navigation'
 import Simples from './componentes/Simples'
 import ParImpar from './componentes/ParImpar'
 import { Inverter, MegaSena } from './componentes/Multi'
+import Contador from './componentes/Contador'
 
 export default createDrawerNavigator({
+    Contador: {
+        screen: () => <Contador numeroInicial={100}/>
+    },
     MegaSena: {
         screen: () => <MegaSena numeros={6} />,
         navigationOptions: { title: 'Mega Sena' }
