9b9cf1eceb40c0b1b80be1b7d493fa35aaf3bd68
paulogermanopg <paulogermano.123pg@hotmail.com>
2020-02-21 13:36:34 -0300

    +

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { View, StyleSheet } from 'react-native'
 
 import Simples from './componentes/Simples'
 import ParImpar from './componentes/ParImpar'
+import { Inverter, MegaSena } from './componentes/Multi'
 
 export default class App extends Component{
     render() {
@@ -10,6 +11,8 @@ export default class App extends Component{
             <View style={styles.container}>
                 <Simples texto='Componente Simples com propriedade!'/>
                 <ParImpar numero={31} />
+                <Inverter texto='Subi no onibus' />
+                <MegaSena numeros={6} />
             </View>
         )
     }
